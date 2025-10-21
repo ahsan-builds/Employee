c1883c5fdcd6cc345d3a8c2a1090dd828bcd6335
ahsan-builds <ahsanazhar029@gmail.com>
2025-10-21 20:39:26 +0500

    Admin

--- Management_System/src/App.jsx
+++ Management_System/src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import './index.css'
 import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
+import AdminDashboard from './components/Dashboard/AdminDashboard'
 const App = () => {
   return (
     <>
     {/* <Login/> */}
-    <EmployeeDashboard/>
+    {/* <EmployeeDashboard/> */}
+    <AdminDashboard/>
     </>
   )
 }
---
+++ Management_System/src/components/Dashboard/AdminDashboard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../Other/Header";
+
+const AdminDashboard = () => {
+  return (
+    <div className="bg-black min-h-screen w-screen">
+      <div className="container mx-auto p-6">
+        <Header />
+      </div>
+    </div>
+  );
+};
+
+export default AdminDashboard;
